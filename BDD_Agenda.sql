f4345ed4196252b2afa38833c1bd1eca6ec780c7
Pikasamurai <43468159+Pikasamurai@users.noreply.github.com>
2019-12-06 15:40:17 +0100

    Add files via upload

--- BDD_Agenda.sql
+++ BDD_Agenda.sql
@@ -1,7 +1,7 @@
 #------------------------------------------------------------
 #        Script MySQL.
 #------------------------------------------------------------
-drop database if exists BDD_Agenda ;
+drop database if exists BDD_Agenda;
 create database BDD_Agenda;
 use BDD_Agenda;
 
@@ -13,7 +13,7 @@ CREATE TABLE Username(
     id_user Int  Auto_increment  NOT NULL ,
     name_user Varchar (50) NOT NULL ,
     password   Varchar (50) NOT NULL
-	,CONSTRAINT User_PK PRIMARY KEY (iduser)
+	,CONSTRAINT User_PK PRIMARY KEY (id_user)
 )ENGINE=InnoDB;
 
 #------------------------------------------------------------
@@ -25,8 +25,8 @@ CREATE TABLE Task(
 	id_taskuser_fk int NOT NULL ,
 	name_task Varchar (50) NOT NULL ,
 	comment_task  text (200) NOT NULL ,
-	date_task DATETIME() NOT NULL ,
-	,CONSTRAINT Task_PK PRIMARY KEY (idtask)
+	date_task DATETIME NOT NULL ,
+	CONSTRAINT Task_PK PRIMARY KEY (id_task)
 )ENGINE=InnoDB;
 
 #------------------------------------------------------------
@@ -39,7 +39,7 @@ CREATE TABLE Contact(
 	name_contact Varchar (50) NOT NULL ,
 	adress_contact   Varchar (50) NOT NULL ,
 	phone_contact varchar(50) NOT NULL ,
-	,CONSTRAINT Contact_PK PRIMARY KEY (id_contact)
+	CONSTRAINT Contact_PK PRIMARY KEY (id_contact)
 )ENGINE=InnoDB;
 
 #------------------------------------------------------------
@@ -48,7 +48,7 @@ CREATE TABLE Contact(
 
 CREATE TABLE Note(
 	id_note Int  Auto_increment  NOT NULL ,
-	id_noteuser_fk int auto_increment NOT NULL ,
+	id_noteuser_fk int NOT NULL ,
 	title_note Varchar (50) NOT NULL ,
 	comment_note  text (200) NOT NULL
 	,CONSTRAINT Note_PK PRIMARY KEY (id_note)
@@ -58,9 +58,14 @@ CREATE TABLE Note(
 # Constraints
 #------------------------------------------------------------
 
-CONSTRAINT Task_User_FK FOREIGN KEY (id_taskuser_fk) REFERENCES Username(iduser) ON DELETE CASCADE
-CONSTRAINT Contact_User_FK FOREIGN KEY (id_contactuser_fk) REFERENCES Username(iduser) ON DELETE CASCADE
-CONSTRAINT Note_User_FK FOREIGN KEY (id_noteuser_fk) REFERENCES Username(iduser) ON DELETE CASCADE
+ALTER TABLE Task
+ADD FOREIGN KEY (id_taskuser_fk) REFERENCES Username(id_user) ON DELETE CASCADE;
+
+ALTER TABLE Contact
+ADD FOREIGN KEY (id_contactuser_fk) REFERENCES Username(id_user) ON DELETE CASCADE;
+
+ALTER TABLE Note
+ADD FOREIGN KEY (id_noteuser_fk) REFERENCES Username(id_user) ON DELETE CASCADE;
 
 #------------------------------------------------------------
 # Insert : Username
@@ -76,25 +81,25 @@ insert into Username values (null, "tete", "tete");
 # Insert : Task
 #------------------------------------------------------------
 
-insert into Task values (null, null, "task1", "c'est la task1", CURRENT_TIMESTAMP);
-insert into Task values (null, null, "task2", "c'est la task2", CURRENT_TIMESTAMP);
-insert into Task values (null, null, "task3", "c'est la task3", CURRENT_TIMESTAMP);
-insert into Task values (null, null, "task4", "c'est la task4", CURRENT_TIMESTAMP);
+insert into Task values (null, 1, "task1", "c'est la task1", CURRENT_TIMESTAMP);
+insert into Task values (null, 2, "task2", "c'est la task2", CURRENT_TIMESTAMP);
+insert into Task values (null, 3, "task3", "c'est la task3", CURRENT_TIMESTAMP);
+insert into Task values (null, 2, "task4", "c'est la task4", CURRENT_TIMESTAMP);
 
 #------------------------------------------------------------
 # Insert : Contact
 #------------------------------------------------------------
 
-insert into Contact values (null, null, "Contact1", "c'est le contact1", CURRENT_TIMESTAMP);
-insert into Contact values (null, null, "Contact2", "c'est le contact2", CURRENT_TIMESTAMP);
-insert into Contact values (null, null, "Contact3", "c'est le contact3", CURRENT_TIMESTAMP);
-insert into Contact values (null, null, "Contact4", "c'est le contact4", CURRENT_TIMESTAMP);
+insert into Contact values (null, 1, "Contact1", "c'est le contact1", CURRENT_TIMESTAMP);
+insert into Contact values (null, 2, "Contact2", "c'est le contact2", CURRENT_TIMESTAMP);
+insert into Contact values (null, 3, "Contact3", "c'est le contact3", CURRENT_TIMESTAMP);
+insert into Contact values (null, 4, "Contact4", "c'est le contact4", CURRENT_TIMESTAMP);
 
 #------------------------------------------------------------
 # Insert : Note
 #------------------------------------------------------------
 
-insert into Note values (null, null, "Note1", "c'est la note1", CURRENT_TIMESTAMP);
-insert into Note values (null, null, "Note2", "c'est la note2", CURRENT_TIMESTAMP);
-insert into Note values (null, null, "Note3", "c'est la note3", CURRENT_TIMESTAMP);
-insert into Note values (null, null, "Note4", "c'est la note4", CURRENT_TIMESTAMP);
+insert into Note values (null, 1, "Note1", "c'est la note1");
+insert into Note values (null, 2, "Note2", "c'est la note2");
+insert into Note values (null, 3, "Note3", "c'est la note3");
+insert into Note values (null, 4, "Note4", "c'est la note4");
